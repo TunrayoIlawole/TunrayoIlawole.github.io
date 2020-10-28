0e59efaed76cbb4f86c8e3f0f54cac82ae48c31e
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2020-10-29 00:15:02 +0100

    add functionality to login page

---
+++ Task 8/script1.js
@@ -0,0 +1,74 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submit = document.querySelector('button');
+
+function showSuccess(input) {
+    const formInput = input.parentElement;
+    // formInput.classList.add('success');
+    formInput.className = 'form-input success';
+}
+
+function showError(input, message) {
+    const formInput = input.parentElement;
+    // formInput.classList.add('error');
+    formInput.className = 'form-input error';
+    const small = formInput.querySelector('small');
+    small.textContent = message;
+}
+
+function checkEmail(input) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regex.test(input.value.trim())) {
+        showSuccess(input);
+        return true;
+    }
+    else {
+        showError(input, 'Email is not valid');
+        return false
+    }
+}
+
+// Check Password 
+function checkPassword(input) {
+    if(input.value.length < 8) {
+        showError(input, 'Password must be at least 8 characters long');
+        return false;
+    }
+    else {
+        showSuccess(input);
+        return true;
+    }
+}
+
+
+const validInputs = [];
+
+[email, password].forEach(input => {
+    input.addEventListener('change', () => {
+        if(input.required) {
+            let isValid;
+            if (input.type === 'email') {
+                isValid = checkEmail(input);
+            }
+            else if (input.type === 'password') {
+                isValid = checkPassword(input);
+            }
+
+
+            if(isValid) {
+                validInputs.push(input);
+            }
+
+
+            if (validInputs.length === 2) {
+                submit.disabled = false;
+            }
+            else {
+                submit.disabled = true;
+            }
+
+            console.log(validInputs);
+        }
+    })
+});
