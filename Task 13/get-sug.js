02c0b0acdc43d7668555653c3bd19d3ca98448f7
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2020-12-21 06:36:20 +0100

    create function to fetch suggestions

--- Task 13/get-sug.js
+++ Task 13/get-sug.js
@@ -3,3 +3,68 @@ const domElements5 = {
     itemsCon: document.querySelector('.items-con'),
     noItems: document.querySelector('h2')
 }
+
+const fetchSuggestions = (category = '') => {
+    window.makeRequest.get(`/suggested${category}`)
+      .then(response => {
+        const suggestions = response.data;
+        console.log(suggestions);
+
+        // display suggestions
+        suggestions.forEach(suggestion => {
+          const item = document.createElement('div');
+          item.classList.add("item");
+
+          const heading = document.createElement("h3");
+          heading.textContent = suggestion.item_category;
+
+          const itemDetails = document.createElement("div");
+          itemDetails.classList.add("item-details");
+
+          const itemHeadings = document.createElement("div");
+          itemHeadings.classList.add("item-headings");
+
+          const headingOne = document.createElement("p");
+          headingOne.textContent = "Name:";
+          const headingTwo = document.createElement("p");
+          headingTwo.textContent = "Desc:";
+          const headingThree = document.createElement("p");
+          headingThree.textContent = "Reason:";
+
+          itemHeadings.appendChild(headingOne);
+          itemHeadings.appendChild(headingTwo);
+          itemHeadings.appendChild(headingThree);
+
+          const itemDes = document.createElement("div");
+          itemDes.classList.add("item-nouns");
+
+          const detailOne = document.createElement("p");
+          detailOne.textContent = `${suggestion.item_name}`;
+          const detailTwo = document.createElement("p");
+          detailTwo.textContent = `${suggestion.item_description}`;
+          const detailThree = document.createElement("p");
+          detailThree.textContent = `${suggestion.reason}`;
+
+          itemDes.appendChild(detailOne);
+          itemDes.appendChild(detailTwo);
+          itemDes.appendChild(detailThree);
+
+          itemDetails.append(itemHeadings);
+          itemDetails.append(itemDes);
+
+          item.append(heading);
+          item.append(itemDetails);
+
+          domElements5.itemsCon.append(item);
+
+        });
+
+        if (suggestions.length === 0) {
+          domElements5.noItems.classList.add('show');
+        }
+        else {
+          domElements5.noItems.classList.remove('show');
+        }
+
+      });
+};
